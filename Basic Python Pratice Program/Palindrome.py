c408360c1b7d9fbdaa031d7f8cc99b74b460842e
nisharachel <73634754+nisharachel@users.noreply.github.com>
2022-10-07 21:06:36 +0530

    Create Palindrome.py

---
+++ Basic Python Pratice Program/Palindrome.py
@@ -0,0 +1,19 @@
+str = 'JaVaJ'  
+
+strstr = str.casefold()  
+
+  
+
+# This string is reverse.  
+
+rev = reversed(str)  
+
+  
+
+if list(str) == list(rev):  
+
+   print("PALINDROME !")  
+
+else:  
+
+   print("NOT PALINDROME !")  
